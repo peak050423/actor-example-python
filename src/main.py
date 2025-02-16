f137699598b6e96a607275cc24e8e307744b5768
martin-ezequiel-barrera <barrea871009@gmail.com>
2025-02-16 14:55:43 +0100

    push main functions

--- src/main.py
+++ src/main.py
@@ -1,5 +1,19 @@
 from apify import Actor
+from datetime import datetime
+import platform
+import io
+import csv
+from src import getFollowerList, getLikersList
 
+def get_current_timestamp():
+    """
+    Get current timestamp (cross-platform).
+    """
+    current_datetime = datetime.now()
+    if platform.system() == "Windows":
+        return current_datetime.strftime('%#m-%d-%Y_%#I-%M-%p')
+    else:
+        return current_datetime.strftime('%-m-%d-%Y_%-I-%M-%p')
 
 async def main():
     async with Actor:
@@ -13,9 +27,45 @@ async def main():
         print(f'Company_url: {company_url}')
         print(f'Follower number: {follower_number}')
 
-        result = f"{company_url} + {follower_number}"
+        current_timestamp = get_current_timestamp()
+        result = []
+        filename = ""
+        scraper_type = "1"
 
-        print(f'The result is: {result}')
+        if scraper_type == '1':  # Company Followers Scraper
+            company_url = input.get('companyUrl')
+            follower_number = input.get('followerNumber')
+
+            if not company_url or not follower_number:
+                raise ValueError('Missing required parameters for company scraper')
+
+            company_id = company_url.split('/')[4]
+            followers_info = getFollowerList(company_id, follower_number, current_timestamp)
+
+            if not followers_info:
+                raise ValueError('No data found')
+
+            result = followers_info
+            filename = f'followers_data_{current_timestamp}.csv'
+
+        elif scraper_type == '2':  # Post Like Scraper
+            post_url = input.get('postUrl')
+
+            if not post_url:
+                raise ValueError('Missing postUrl for post-like scraper')
+
+            likers_info = getLikersList(post_url, current_timestamp)
+
+            if not likers_info:
+                raise ValueError('No data found')
+
+            result = likers_info
+            filename = f'likers_data_{current_timestamp}.csv'
+
+        else:
+            raise ValueError('Invalid scraper type')
+
+            print(f'The result is: {result}')
 
         # Structure of output is defined in .actor/actor.json
         await Actor.push_data([
