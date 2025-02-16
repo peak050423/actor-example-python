7d85e0fddc6db3d309cf790d72b25c6a75658302
martin-ezequiel-barrera <barrea871009@gmail.com>
2025-02-16 19:48:34 +0100

    remove follower_number from required fields and refactor load_cookies to sync function

--- src/cookie_loader.py
+++ src/cookie_loader.py
@@ -1,18 +1,12 @@
 import json
-from apify import Actor
 
-
-async def load_cookies(file_path='linkedin_cookies.json'):
+def load_cookies(file_path='linkedin_cookies.json'):
     """
     Load cookies from a JSON file and return as a dictionary.
     """
-    async with Actor:
-        actor_input = await Actor.get_input() or {}
-
-        # with open(file_path, 'r') as file:
-        #     linkedin_cookies = json.load(file)
-
-        cookies = actor_input.get('cookies')
-        
-        cookies = {cookie['name']: cookie['value'] for cookie in cookies}
-        return cookies
+    with open(file_path, 'r') as file:
+        linkedin_cookies = json.load(file)
+    
+    
+    cookies = {cookie['name']: cookie['value'] for cookie in linkedin_cookies}
+    return cookies
