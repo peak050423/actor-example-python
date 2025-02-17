11f02dff168d9e65f7e7a60d1152e2723b46d411
martin-ezequiel-barrera <barrea871009@gmail.com>
2025-02-17 16:41:36 +0100

    fix: enhance experience data validation to prevent errors

--- src/data_processor.py
+++ src/data_processor.py
@@ -87,7 +87,7 @@ def get_experience_datas(profile_urns, follower_number, cookies):
         records = []
     
         for url, experience_data in experience_datas.items():
-            if experience_data["included"]:
+            if experience_data and "included" in experience_data and experience_data["included"]:
                 componentCount = sum(1 for item in experience_data["included"] if "components" in item and item["components"])
             if componentCount > 1:
                 for component in experience_data["included"]:
